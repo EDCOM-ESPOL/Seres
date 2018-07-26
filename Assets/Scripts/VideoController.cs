d0122e9c560e9ac553e71954f8a7f78d64fb58f0
Fernando Balladares <balladares.fernando.92@gmail.com>
2018-07-26 10:54:16 -0500

    0.3

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class VideoController : MonoBehaviour {
+
+    //private Button backButton;
+    //private GameObject backButton;
+    //private GameObject activityButton;
+    private GameObject toggler;
+
+    // Use this for initialization
+    void Start () {
+        //backButton = GameObject.Find("BackButton");
+        //activityButton = GameObject.Find("GoToActivityButton");
+        toggler = GameObject.Find("Control Panel");
+        Debug.Log(toggler.activeSelf);
+        toggler.SetActive(false);
+        Debug.Log(toggler.activeSelf);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (toggler.activeSelf == false)
+            {
+                toggler.SetActive(true);
+            }
+            else
+            {
+                toggler.SetActive(false);
+            }
+
+            Debug.Log(toggler.activeSelf);
+        }
+    }
+
+    //void OnMouseDown()
+    //{
+    //    Debug.Log(toggler.activeSelf);
+    //}
+
+
+}
