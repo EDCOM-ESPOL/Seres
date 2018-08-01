f3f6d12c7272a6679a6931eaa2e9c0e54449d8af
Fernando Balladares <balladares.fernando.92@gmail.com>
2018-08-01 03:30:50 -0500

    v0.6.1

--- Assets/Scripts/ActivityController.cs
+++ Assets/Scripts/ActivityController.cs
@@ -7,20 +7,34 @@ public class ActivityController : MonoBehaviour {
 
     public Sprite right;
     public Sprite wrong;
+    public Text orderText;
+    public Text scoreText;
+
+    private string[] order = {"Ser No Vivo", "Ser Vivo" }; //0 = no vivo ... 1 = vivo
+    private int score = 0;
+    
 
 
     // Use this for initialization
     void Start () {
-        
+        scoreText.text = score.ToString();
+        int index = Random.Range(0, 2);
+        orderText.text = order[index];
+
+        gameObject.GetComponent<SpawnActivityOption>().CreateNonLiving();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        scoreText.text = score.ToString();
+        
+    }
 
     public void EvaluateOnClick(Button buttonClicked)
     {
         buttonClicked.GetComponent<Image>().sprite = right;
+        score++;
+        scoreText.text = score.ToString();
     }
 }
---
+++ Assets/Scripts/SpawnActivityOption.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnActivityOption : MonoBehaviour {
+
+    public GameObject activityOptionPrefab;
+    public Sprite[] nonLivingSprites;
+    public Sprite[] livingSprites;
+
+    void Start()
+    {
+        
+    }
+
+    public void CreateNonLiving()
+    {
+        Sprite nonLivingSprite = nonLivingSprites[Random.Range(0, nonLivingSprites.Length)];
+        
+
+        GameObject newActivityOption = Instantiate (activityOptionPrefab);
+        newActivityOption.name = nonLivingSprite.name;
+        newActivityOption.GetComponent<ActivityOption>().isAlive = false;
+        newActivityOption.GetComponent<Image>().sprite = nonLivingSprite;
+        newActivityOption.transform.SetParent(GameObject.Find("OptionPanel").transform);
+        newActivityOption.transform.localScale = new Vector3(1f, 1f, 1f);
+
+
+    }
+
+    public void CreateLiving()
+    {
+        Sprite livingSprite = livingSprites[Random.Range(0, livingSprites.Length)];
+        //bool isAlive = true;
+    }
+}
